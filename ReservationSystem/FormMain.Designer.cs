e3f40caf107fbe82bef0d3e01e6377ba6870b7e5
Ethan_Vodka <gohho1008@gmail.com>
2023-03-10 00:14:00 +0900

    add : project reservation System

--- ReservationSystem/FormMain.Designer.cs
+++ ReservationSystem/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ReservationSystem
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1121, 662);
+            this.Name = "FormMain";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
